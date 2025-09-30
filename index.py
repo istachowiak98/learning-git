375df9856507ccfbdfcf58308430aa23d6e6cbe9
Izabela Stachowiak <i.stachowiak98@gmail.com>
2025-09-30 19:44:14 +0200

    Update from local

--- index.py
+++ index.py
@@ -1,1 +1,15 @@
-print("Hiszpańska inkwizycja to najlepszy skecz grupy Month Pythona")
+a = -100
+b = -10
+c = -6
+delta = b**2-4*a*c
+x1 = (-b-delta)/2*a
+x2 = (-b+delta)/2*a
+x0 = -b/2*a
+if delta > 0:
+  print(x1, x2)
+if delta == 0:
+  print(x0)
+if delta<0:
+  print("Brak")
+  print("coś nowego")
+  
